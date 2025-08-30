674c0abfc17c17424ddd3df0ca8e74222aee2b48
djmaluy <a.tsvirko@ukr.net>
2025-08-30 21:09:02 +0300

    added models specs

--- api/spec/models/membership_spec.rb
+++ api/spec/models/membership_spec.rb
@@ -1,14 +1,5 @@
 RSpec.describe Membership, type: :model do
-  describe "associations" do
-    it { should belong_to(:workspace) }
-    it { should belong_to(:user) }
-  end
-
-
-  describe "enums" do
-    it do
-      should define_enum_for(:role).
-        with_values(admin: 0, member: 1)
-    end
-  end
+  it { should belong_to(:workspace) }
+  it { should belong_to(:user) }
+  it { should define_enum_for(:role) }
 end
